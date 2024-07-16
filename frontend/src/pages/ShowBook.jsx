2f57df34d4cf7a5ec8a4d791169ec73674ba2c72
PabloFC <pablofc_68@hotmail.com>
2024-07-17 00:48:34 +0200

    create book page

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -26,14 +26,14 @@ const ShowBook = () => {
   return (
     <div className="p-4">
       <BackButton />
-      <h1 className="text-3xl">Show Book</h1>
+      <h1 className="text-3xl mt-2">Show Book</h1>
       {loading ? (
         <Spinner />
       ) : (
         <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
           <div className="my-4">
             <span className="text-xl mr-4 text-gray-500">Id</span>
-            <span>{book.id}</span>
+            <span>{book._id}</span>
           </div>
           <div className="my-4">
             <span className="text-xl mr-4 text-gray-500">Title</span>
